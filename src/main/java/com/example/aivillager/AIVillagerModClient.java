b85af7ec9f18495d7e60bf5756c1088169f430b4
Anger-leleda <118098229+Anger-leleda@users.noreply.github.com>
2026-06-17 22:13:56 +0800

    更新 AIVillagerModClient.java

--- src/main/java/com/example/aivillager/AIVillagerModClient.java
+++ src/main/java/com/example/aivillager/AIVillagerModClient.java
@@ -1,1 +1,16 @@
+package com.example.aivillager;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.VillagerEntityRenderer;
+import net.minecraft.client.render.entity.WanderingTraderEntityRenderer;
+
+public class AIVillagerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        EntityRendererRegistry.register(AIVillagerMod.AI_VILLAGER,
+            (context) -> new VillagerEntityRenderer(context));
+        EntityRendererRegistry.register(AIVillagerMod.AI_WANDERING_TRADER,
+            (context) -> new WanderingTraderEntityRenderer(context));
+    }
+}
